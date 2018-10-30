699a9e72366253aa28377fce38b4bc9c83c492e5
Jordan Haddadi <jordanhaddadi@gmail.com>
2018-10-30 14:26:45 -0400

    added js min number drill

--- jsdrills.js
+++ jsdrills.js
@@ -23,6 +23,25 @@ function max(numbers) {
     let array = []
     console.log(max(array))
 
-// function min(numbers) {
+    function min(numbers) {
 
-// }
+        if (numbers.length === 0) {
+            return null;
+        }
+    
+        //declaure a new variable that is set to the first value in array
+        let min = numbers[0];
+        let i = 0;
+        //loop over with while loop to compare values to the max variable
+        while (i > numbers.length) {
+                if (numbers[i] < min) {
+                    min = numbers[i];
+                };
+                i++;
+        }
+        return min
+    
+    }
+
+    let arrayMin = [1, 2, 3, 4]
+    console.log(min(arrayMin))
